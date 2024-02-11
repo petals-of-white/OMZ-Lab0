9bfea0fc35158d748e5a31c140e98874c6500d14
Maksym Syvash <maxim.sivash@gmail.com>
2024-02-12 01:43:05 +0200

    fix hoora

--- src/HelloTriangle.hs
+++ src/HelloTriangle.hs
@@ -3,6 +3,7 @@ module Main where
 import           Control.Monad               (unless)
 import           Graphics.GPipe
 import qualified Graphics.GPipe.Context.GLFW as GLFW
+import Control.Arrow (Arrow(first))
 
 main :: IO ()
 main =
@@ -17,7 +18,15 @@ main =
 
     shader <- compileShader $ do
       primitiveStream <- toPrimitiveStream id
-      fragmentStream <- rasterize (const (FrontAndBack, ViewPort (V2 0 0) (V2 500 500), DepthRange 0 1)) primitiveStream
+      let primitiveStream2 = fmap (\(pos,clr) -> (pos - V4 1 1 0 0, clr / 10)) primitiveStream  
+      let primitiveStream3 = primitiveStream <> primitiveStream2  
+      let rotationMatrix a = V4 (V4 (cos a) (-sin a) 0 0)   
+                                (V4 (sin a) (cos a) 0 0)  
+                                (V4 0    0    1 0)  
+                                (V4 0    0    0 1)  
+      let primitiveStream4 = fmap (first (rotationMatrix (-0.2) !*)) primitiveStream3   
+                      
+      fragmentStream <- rasterize (const (FrontAndBack, ViewPort (V2 0 0) (V2 500 500), DepthRange 0 1)) primitiveStream4
       drawWindowColor (const (win, ContextColorOption NoBlending (V3 True True True))) fragmentStream
 
     loop vertexBuffer shader win
